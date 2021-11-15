8e33181650ea5238a2d6e598cc05663bd65ea919
Artyom <94391520+artemsidorenko1996@users.noreply.github.com>
2021-11-15 21:24:20 +0300

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Artyom on 15.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
